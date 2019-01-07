1f19dc8c4b75f50ad3e035df0c56fccdeb9e13b1
Mike Müller <MarsMike@users.noreply.github.com>
2019-01-07 21:06:16 +0100

    Update App.java

--- src/main/java/com/issc29/examples/App.java
+++ src/main/java/com/issc29/examples/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        TextDecorator a = new MyNewErrorTextDecorator("Hello World from Mike");
+        TextDecorator a = new TextDecorator("Hello World from Mike, now fixed ;-)");
         System.out.println(a.doubleString());
         System.out.println(a.pipe());
         System.out.println(a.getText());
